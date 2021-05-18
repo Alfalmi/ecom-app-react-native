6fddac61f17b72514240dd60000081ae8567db83
pinooxd <pinooxd@gmail.com>
2021-05-18 13:14:48 -0700

    ok

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Login from './Login';
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import Register from './Register';
+import { StackActions } from '@react-navigation/routers';
+import { Routes} from './stackRoutes'
+
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+    <Stack.Navigator>
+    <Stack.Screen name="Home" component={Routes.Home} />
+    <Stack.Screen name="Login" component={Routes.Login} />
+    <Stack.Screen name="Register" component={Routes.Register} />
+    </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
